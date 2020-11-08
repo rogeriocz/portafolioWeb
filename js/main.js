6db033c90c3e39b1d20c4a53a62e2e7c6ee04b4c
rogeriocz <rogeriocz@gmail.com>
2020-11-08 15:20:31 -0600

    se agrega navidad y pokemon

--- js/main.js
+++ js/main.js
@@ -1,42 +1,84 @@
-  
-  
-  const fecha = new Date();
-  //let mes = fecha.getMonth() +1;
-  let ocultar = document.getElementById("contenedor-imagen");
-  let mostrar = document.getElementById("contenedor-zombie");
+  /* Cambia la imagen principal por la de zombie siempre y cuando
+  la fecha sea 1 y 2 de noviembre */
 
-  if (fecha.getMonth()+1 == 11 && fecha.getDate() <= 2){
-    ocultar.style.display="none";
-    mostrar.style.display="";
-   
-  }else{
-    ocultar.style.display="";
-    mostrar.style.display="none";
-    input.value="Zombie"
+  const fecha = new Date();
+  let ocultarDev = document.getElementById("contenedor-imagen");
+  let pokemon = document.getElementById("pokemon");
+  let mostrarZombie = document.getElementById("contenedor-zombie");
+  const descripcion = document.getElementById("descripcion");
+  const mensaje = document.getElementById("mensaje");
+  const btnOpcion = document.getElementById("btnOpcion");
+  //const imagenes = document.getElementById("imagenes");
+
+
+  function navidad(){
+    ocultarDev.src = "./img/navidad.png";
+    mensaje.innerHTML="Hey!! Feliz Navidad";
+    ocultarDev.style.display="";
   }
 
+  function rogerioPokemon(){
+    pokemon.src = "./img/pokemon.png";
+    mensaje.innerHTML="Hola!! Hijo";
+    ocultarDev.style.display="";
+   }
 
+  function devRogerio(){
+    ocultarDev.style.display="";
+    mostrarZombie.style.display="none";
+    ocultarDev.src = "./img/premium_15.png"; //agregar imagen cuando necesite cambiarla por otra
+    
+  }
 
+ 
 
+  function diaMuertos(){
+      ocultarDev.style.display="none";
+      mostrarZombie.style.display="";
+      descripcion.innerHTML="Hey!! Presiona el Botón de Vacuna";
+      descripcion.style.color = "#fb5607";
+      btnOpcion.style.visibility = "visible";
+      btnOpcion.value="Vacuna";
+  }
+
+  /* function birthdayHija({
+    
+  }) */
+ 
+
+ function eventosMensuales(){
+
+  if (fecha.getMonth()+1 == 11 && fecha.getDate() <= 2){
+    diaMuertos();
+    }else if(fecha.getMonth()+1 == 12 && fecha.getDate() == 24){
+      navidad();
+    }else if(fecha.getMonth()+1 == 11 && fecha.getDate() == 8){
+      rogerioPokemon();
+    }else{
+      devRogerio();
+  }
+
+ }
+
+ eventosMensuales();
 
   function mostrar(input) {
-    let ocultar = document.getElementById("contenedor-imagen");
-    let mostrar = document.getElementById("contenedor-zombie");
-    
-  
+   
       if(input.value=="Zombie")
       {
-        ocultar.style.display="none";
-        mostrar.style.display="";
+        ocultarDev.style.display="none";
+        mostrarZombie.style.display="";
         
         input.value="Vacuna"
         
       }else{
-        ocultar.style.display="";
-        mostrar.style.display="none";
+        ocultarDev.style.display="";
+        mostrarZombie.style.display="none";
+        descripcion.innerHTML="Desarrollador Web, Back-End.";
+        descripcion.style.color = "#fff";
         
         input.value="Zombie"
-      }
+      } 
   
       
   }
